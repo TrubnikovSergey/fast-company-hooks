771718f623b31aee1fd7c5c9c951f50b058cbf16
Trubnikov Sergey <trubnikovsergey1@gmail.com>
2022-09-07 13:36:33 +0300

    beginer projec loaded

--- src/app/components/examples/useRef/prevState.jsx
+++ src/app/components/examples/useRef/prevState.jsx
@@ -1,38 +1,29 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import CardWrapper from "../../common/Card";
+import Divider from "../../common/divider";
 import SmallTitle from "../../common/typografy/smallTitle";
 const PrevStateExample = () => {
-    const prevState = useRef("");
-    const [otherState, setOtherState] = useState("false");
-    const toggleOtherState = () => {
-        setOtherState((prevState) => {
-            console.log("setOtherState =", otherState);
-            return prevState === "false" ? "true" : "false";
-        });
-    };
-    useEffect(() => {
-        prevState.current = otherState;
-        console.log("useEffect otherState =", otherState);
-        console.log("useEffect prevState.current =", prevState.current);
-    }, [otherState]);
-
-    // console.log("return otherState =", otherState);
-    // console.log("return prevState.current =", prevState.current);
-    return (
-        <CardWrapper>
-            <SmallTitle>Предыдущее состояние</SmallTitle>
-            <p>prev State:{prevState.current}</p>
-            <p>Current State:{otherState}</p>
-            <p>Log:{console.log("JSX otherState =", otherState)}</p>
-            <p>
-                Log:
-                {console.log("JSX prevState.current =", prevState.current)}
-            </p>
-            <button className="btn btn-primary" onClick={toggleOtherState}>
-                Toggle other state
-            </button>
-        </CardWrapper>
+  const prevState = useRef("");
+  const [otherState, setOtherState] = useState("false");
+  const toggleOtherState = () => {
+    setOtherState((prevState) =>
+      prevState === "false" ? "true" : "false"
     );
+  };
+  useEffect(() => {
+    prevState.current = otherState;
+  }, [otherState]);
+  return (
+    <CardWrapper>
+      <SmallTitle>Предыдущее состояние</SmallTitle>
+      <Divider />
+      <p>prev State:{prevState.current}</p>
+      <p>Current State:{otherState}</p>
+      <button className="btn btn-primary" onClick={toggleOtherState}>
+        Toggle other state
+      </button>
+    </CardWrapper>
+  );
 };
 
 export default PrevStateExample;
